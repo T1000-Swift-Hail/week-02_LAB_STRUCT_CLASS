fe9d47ab911157a614cdb1d3d7a4fb93ddf7eb5c
Munira abdullah <muniraabdullah@MacBook-alkhas-b-Munira.local>
2021-10-12 14:08:11 +0300

    Done N

--- LAB_STRUCT_CLASS.playground/Contents.swift
+++ LAB_STRUCT_CLASS.playground/Contents.swift
@@ -1,18 +1,23 @@
-import UIKit
+
 struct City {
     var name : String
     var population : Int
     var location : String
     var geology : String
 var weather : String
-}
- func NameCity (Hail: String) {
-}
+ func printDescription (name: String ,population : Int,location : String,geology : String,weather : String) {
+     print("name city \(name),and population \(population) and location\(location) and geology \(geology)and  weather \( weather) ")
+ }}
 class Country {
-    var name : String = String ()
+    var name : String =  ""
     var cities : [City] = []
+    
     func printDescription () {
-       
+      
+        for city in cities {
+    
+            city.printDescription ( name: city.name , population: city.population, location: city.location , geology: city.geology , weather: city.weather )
+        }}}
     let FirstCities = City (name: "Hail" , population: 20000 , location: "North of Saudi Arabia ", geology: "Samra mountain", weather: "Good all time")
     
     let SecondCities = City (name: "Riyadh" , population: 90000 , location: "Center of Saudi Arabia ", geology: "Tuwaiq mountain", weather: "Hot in summer and cold in winter")
@@ -23,7 +28,10 @@ class Country {
         
     let FifeCities = City (name: "Madenah" , population: 500000 , location: "Wast of Saudi Arabia ", geology: "Haram", weather: "Hot in summer and cold in winter")
         
-        
-        cities.append(contentsOf: [FirstCities,SecondCities,TherdCities,FordCities,FifeCities])
-    }
-}
+var saudiArabia = Country()
+    saudiArabia.cities.append(contentsOf: [FirstCities,SecondCities,TherdCities,FordCities,FifeCities])
+
+    saudiArabia.printDescription()
+    
+
+
