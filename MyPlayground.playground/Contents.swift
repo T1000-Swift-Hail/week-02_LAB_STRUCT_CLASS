3742e7276f30b9c052cf67098acda9ee3a50fc9c
Monafh <monafh@MacBook-Pro-alkhas-b-Monafh.local>
2021-10-12 14:09:07 +0300

    edit

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -9,28 +9,34 @@ struct City {
     var weather : String
     
     func printDescription(name : String, population : Int, location : String ,geology : String, weather : String  ) {
-        
+        print("city \(name). It is located in the \(location) of Saudi Arabia . It has a population of \(population) million people. It has \(geology) . The weather in \(name)is \(weather)and hot is summer")
     }
 }
 class Country {
-    var name : String = "Saudi Arabia"
+    var name : String = ""
     var cities : [City] = []
     
     func printDescription() {
-        let firstCity = City(name: "Riyadh", population: 80, location: "North", geology: "Mountain", weather: "sunny")
-        let sCity = City(name: "Hail", population: 90, location: "West", geology: "desert", weather: "sunny")
-        let thCity = City(name: "Abha", population: 70, location: "south", geology: "desert", weather: "sunny")
-        let ForCity = City(name: "Jeedh", population: 50, location: "East", geology: "desert", weather: "sunny")
-        let FifCity = City(name: "AlQaseem", population: 60, location: "North", geology: "desert", weather: "sunny")
-        cities.append(contentsOf: [firstCity,sCity,thCity,ForCity,FifCity])
-        print(country.name)
+        
         for city in cities {
-            print(" city \(city.name). It is located in the \(city.location) of Saudi Arabia . It has a population of \(city.population) million people. It has Tuwaiq mountain . The weather in \(city.name)is \(city.weather) is summer, cold in winter.")
-        }}}
+            city.printDescription(name: city.name, population: city.population , location: city.location, geology: city.geology , weather: city.weather)
+        }
+        
+    }
+    
+}
+
+let firstCity = City(name: "Riyadh", population: 80, location: "North", geology: "Mountain", weather: "sunny")
+let sCity = City(name: "Hail", population: 90, location: "West", geology: "desert", weather: "sunny")
+let thCity = City(name: "Abha", population: 70, location: "south", geology: "desert", weather: "sunny")
+let ForCity = City(name: "jeddah", population: 50, location: "East", geology: "desert", weather: "sunny")
+let FifCity = City(name: "AlQaseem", population: 60, location: "North", geology: "desert", weather: "sunny")
+
 let country = Country()
-country.printDescription()
+country.name = "Saudi Arabia"
 print(country.name)
+country.cities.append(contentsOf: [firstCity,sCity,thCity,ForCity,FifCity])
+country.printDescription()
 
 
-// function saudiArabia.printDescription()
 
