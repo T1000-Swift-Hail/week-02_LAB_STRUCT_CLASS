ac06f9495a7396bd679c0cd8a1ae72d9f1ad084f
موضي الحربي <modhialhrbi@MacBook-Air-mwdy.local>
2021-10-12 14:10:12 +0300

    DONE LAB

--- LAB_STRUCT.playground/Contents.swift
+++ LAB_STRUCT.playground/Contents.swift
@@ -1,4 +1,3 @@
-import UIKit
 
 struct City {
 var name : String
@@ -6,37 +5,39 @@ var location : String
 var geology : String
 var weather : String
 var population : Int
-func printDescription(){
-    print("this city name \(name) and population \(population)")
-}
-    
-}
+func printDescription(name : String
+                      , location : String
+                      , geology : String
+                      , weather : String
+                      , population : Int) {
+    print("this city name\(name) and population \(population) and location \(location) and geology \(geology) and weather \(weather ) ")
+}}
 
 class Country {
-    
+    var name :String = ""
     var cities : [City] = []
     
 func printDescription() {
     
-   let first =  City(name:"Hail", location: " It is located in the north of Saudi Arabia", geology: "It has Aja and Salma mountain ", weather:"the weather in Hail is hot is summer, cold in winter", population:363278)
-    
-    let second = City(name: "Riyadh", location: "It is located in the center of Saudi Arabia ", geology: "It has Tuwaiq mountain", weather: "The weather in Riyadh is hot is summer, cold in winter", population: 5236901)
-    
-    let third = City(name:" Makkah", location: "It is located in the west of Saudi Arabia", geology: "It has hera mountain", weather: "The weather in Makkah is  very hot  summer, mild weather in winter", population: 1578722)
-    
-    let fourth = City(name:" Maddinh", location: "It is located in the west of Saudi Arabia", geology: "It has Uhud mountain", weather: "The weather in Maddinh is  very hot  summer, mild weather cold in winter", population: 1152991)
-                    
-    let fifth = City(name: "Khobar", location: "It is located in the east of Saudi Arabia", geology: "It has The Arabian Gulf", weather: "The weather in Maddinh is  very hot  summer,  very cold in winter", population: 457748)
-    
-    
-    cities.append(contentsOf: [first,second,third,fourth,fifth])
-        
     for city in cities{
-        city.printDescription()
+       
+        city.printDescription(name: city.name, location: city.location, geology: city.geology, weather: city.weather,
+                              population: city.population)
     }
-    }
-}
-
+}}
     
+let first =  City(name:"Hail", location: " It is located in the north of Saudi Arabia", geology: "It has Aja and Salma mountain ", weather:"the weather in Hail is hot is summer, cold in winter", population:363278)
+ 
+ let second = City(name: "Riyadh", location: "It is located in the center of Saudi Arabia ", geology: "It has Tuwaiq mountain", weather: "The weather in Riyadh is hot is summer, cold in winter", population: 5236901)
+ 
+ let third = City(name:" Makkah", location: "It is located in the west of Saudi Arabia", geology: "It has hera mountain", weather: "The weather in Makkah is  very hot  summer, mild weather in winter", population: 1578722)
+ 
+ let fourth = City(name:" Maddinh", location: "It is located in the west of Saudi Arabia", geology: "It has Uhud mountain", weather: "The weather in Maddinh is  very hot  summer, mild weather cold in winter", population: 1152991)
+                 
+ let fifth = City(name: "Khobar", location: "It is located in the east of Saudi Arabia", geology: "It has The Arabian Gulf", weather: "The weather in Maddinh is  very hot  summer,  very cold in winter", population: 457748)
 
+var saudiArabia = Country()
 
+saudiArabia.cities.append(contentsOf: [first,second,third,fourth,fifth])
+saudiArabia.printDescription()
+ 
